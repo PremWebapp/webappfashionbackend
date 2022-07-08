9ecaa79ffa4fbec59b42149069072b504fd75841
PremWebapp <prem@webapp.world>
2022-07-08 18:10:42 +0530

    added

--- routes/routesFIle.js
+++ routes/routesFIle.js
@@ -2,7 +2,15 @@ import express from 'express';
 import multer from 'multer';
 
 const router = express.Router();
-const storage = multer.diskStorage({});
+
+var storage = multer.diskStorage({   
+    destination: function(req, file, cb) { 
+       cb(null, 'uploadedFile');    
+    }, 
+    filename: function (req, file, cb) { 
+       cb(null , file.originalname);   
+    }
+ });
 
 let upload = multer({
     storage
@@ -11,6 +19,8 @@ let upload = multer({
 router.post('/upload', upload.single('myFile'), (req, res) => {
     try {
         if(!req.file) return res.status(404).json({message:'we get no file for upload'});
+        res.send(req.file);
+
         console.log(req.file)
     } catch (error) {
         console.log(error.message);
